3eb7c53fab763e1f6475443c5ac8e3a948c1da31
Xyonox <93388182+Xyonox@users.noreply.github.com>
2023-09-09 00:20:36 +0200

    init - Xtool.java

--- src/main/java/de/xyonox/Xtool.java
+++ src/main/java/de/xyonox/Xtool.java
@@ -1,4 +1,12 @@
 package de.xyonox;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Objects;
+
 public class Xtool {
+    public Xtool(@NotNull Map<String, Object> o){
+        o.get("soul");
+    }
 }
